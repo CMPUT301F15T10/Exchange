c79f16cde4bc8cbba2821d87d24eb439507b445e
k----n <k----n@users.noreply.github.com>
2015-10-09 15:23:16 -0600

    Update ProfileTest.java

--- app/src/androidTest/java/cmput301f15t10/exchange/ProfileTest.java
+++ app/src/androidTest/java/cmput301f15t10/exchange/ProfileTest.java
@@ -1,21 +1,3 @@
-public class Person {
-    public String name;
-    public String location;
-    public int ID;
-    public Picture myPicture;
-    public Long phoneNumber;
-    public String userName;
-    public Inventory myInventory;
-    public String email;
-
-    public Person(String name, int ID, String userName, String email) {
-        this.name = name;
-        this.ID = ID;
-        this.userName = userName;
-        this.email = email;
-    }
-}
-
 package cmput301f15t10.exchange;
 
 public class ProfileTest {
