619d5f6e1b098c5e8dd433e80052b7b1ea5e69cf
Yishuo Wang <yishuo@ualberta.ca>
2015-10-31 20:32:32 -0600

    Email Activity

--- app/src/main/java/cmput301f15t10/exchange/MainActivity.java
+++ app/src/main/java/cmput301f15t10/exchange/MainActivity.java
@@ -1,9 +1,12 @@
 package cmput301f15t10.exchange;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +14,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button testEmailButton = (Button) findViewById(R.id.testEmailButton);
+        testEmailButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SendEmailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/cmput301f15t10/exchange/SendEmailActivity.java
@@ -0,0 +1,59 @@
+package cmput301f15t10.exchange;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Yishuo Wang on 2015/10/31.
+ */
+
+// http://www.mkyong.com/android/how-to-send-email-in-android
+// By: mkyong
+public class SendEmailActivity extends Activity {
+
+    public String sendTo, subject, message;
+    Button sendButton;
+    EditText comment;
+    public TextView text;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sendemail);
+
+        // test
+        message = "YOLO";
+        text = (TextView) findViewById(R.id.messageText);
+        text.setMovementMethod(new ScrollingMovementMethod());
+        text.setText(message);
+
+
+        sendButton = (Button) findViewById(R.id.sendButton);
+        comment = (EditText) findViewById(R.id.commentEditText);
+
+        sendButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
+    }
+
+    private void sendEmail() {
+        message = message + "\n\n" + comment.getText().toString();
+        Intent email = new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_EMAIL, sendTo);
+        email.putExtra(Intent.EXTRA_SUBJECT, subject);
+        email.putExtra(Intent.EXTRA_TEXT, message);
+
+        email.setType("message/rfc822");
+
+        startActivity(Intent.createChooser(email, "Choose an Email client:"));
+    }
+}
