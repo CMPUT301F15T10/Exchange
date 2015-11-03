c41e04c172c6e08221cda9cb7174ce3525ce3d7b
laingsoft <cclaing@ualberta.ca>
2015-11-02 19:30:17 -0700

    Merged Baihong manually. The Commit was giving me problems

---
+++ app/src/main/java/cmput301exchange/exchange/BrowseInventories.java
@@ -0,0 +1,54 @@
+package cmput301f15t10.exchange;
+
+/**
+ * Created by bq on 11/1/15.
+ */
+public interface BrowseInventories {
+    Inventory searchinventory(Friend afriend);
+
+    /*
+    {
+    return afriend.getMyInventory();
+    }
+     */
+    Inventory seaechinventorybycate(Friend afriend, int genreID);//creat a new inventory and put all the result in it and return it
+
+    /*
+   {
+    Inventory result = new Inventory();
+    int n = afriend.getMyInventory().getInventory().size();
+    for(int i=0; i<n;i++){
+     if(afriend.getMyInventory().getInventory().get(i).getGenreID()==genreID){
+     result.getInventory().add(afriend.getMyInventory().getInventory().get(i));
+     }
+    }
+    return result;
+   }
+    */
+    Inventory seaechinventorybytext(Friend afriend, String query);//creat a new inventory and put all the result in it and return it
+        /*
+   {
+    Inventory result = new Inventory();
+    int n = afriend.getMyInventory().getInventory().size();
+    for(int i=0; i<n;i++){
+     if(afriend.getMyInventory().getInventory().get(i).getBookName().toLowerCase().contains(query.toLowerCase())){
+     result.getInventory().add(afriend.getMyInventory().getInventory().get(i));
+     continue
+     }
+
+     if(afriend.getMyInventory().getInventory().get(i).getAuthor().toLowerCase().contains(query.toLowerCase())){
+     result.getInventory().add(afriend.getMyInventory().getInventory().get(i));
+     continue
+     }
+
+     if(afriend.getMyInventory().getInventory().get(i).getPublisher().toLowerCase().contains(query.toLowerCase())){
+     result.getInventory().add(afriend.getMyInventory().getInventory().get(i));
+     continue
+     }
+
+     if(afriend.getMyInventory().getInventory().get(i).getDescription().toLowerCase().contains(query.toLowerCase())){
+     result.getInventory().add(afriend.getMyInventory().getInventory().get(i));
+     continue
+     }*/
+
+}
--- app/src/main/java/cmput301exchange/exchange/Person.java
+++ app/src/main/java/cmput301exchange/exchange/Person.java
@@ -2,6 +2,8 @@ package cmput301f15t10.exchange;
 
 import android.graphics.Picture;
 
+import java.util.ArrayList;
+
 /**
  * Created by Yishuo Wang on 2015/10/5.
  */
@@ -14,17 +16,96 @@ public abstract class Person {
     private String userName;
     private Inventory myInventory;
     private String email;
+    private FriendList myfriendlist;
+    private ArrayList<History> historylist;
+    private ArrayList<Trade> tradelist;
 
-    private Inventory inventory;
 
-    public Person() {
+    public Person(String username) {
+        this.userName =username;
         myInventory = new Inventory(); //Each user has only one Inventory.
     }
 
-    private Inventory getInventory(){return this.inventory;}
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public Picture getMyPicture() {
+        return myPicture;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Inventory getMyInventory() {
+        return myInventory;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 
 
+    public void setHistorylist(ArrayList<History> historylist) {
+        this.historylist = historylist;
+    }
 
+    public void addHistorylist(History event) {
+        this.historylist.add(event);
+    }
+
+    public void setTradelist(ArrayList<Trade> tradelist) {
+        this.tradelist = tradelist;
+    }
+
+    public void addTradelist(Trade trade) {
+        this.tradelist.add(trade);
+    }
+
+    public FriendList getMyfriendlist() {
+        return myfriendlist;
+    }
+
+    public ArrayList<History> getHistorylist() {
+        return historylist;
+    }
+
+    public ArrayList<Trade> getTradelist() {
+        return tradelist;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setMyPicture(Picture myPicture) {
+        this.myPicture = myPicture;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 
 }
