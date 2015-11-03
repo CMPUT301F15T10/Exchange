0ac323fb02e25cc8338b609cd060033272b0f396
k----n <kalvin@kalvineng.com>
2015-11-03 13:08:21 -0700

    Navigation from HomeActivity works

--- app/src/main/java/cmput301exchange/exchange/Activities/InventoryActivity.java
+++ app/src/main/java/cmput301exchange/exchange/Activities/InventoryActivity.java
@@ -1,22 +1,32 @@
 package cmput301exchange.exchange.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import cmput301exchange.exchange.R;
+
+
 public class InventoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_inventory, menu);
+
+        // http://developer.android.com/training/appbar/action-views.html
+        // Get the SearchView and set the searchable configuration
         return true;
     }
 
