e5203a742f7c02675db58c9ac007e925ff9f5afa
laingsoft <cclaing@ualberta.ca>
2015-11-05 16:23:45 -0700

    Added javadoc for Book.

--- app/src/main/java/cmput301exchange/exchange/Book.java
+++ app/src/main/java/cmput301exchange/exchange/Book.java
@@ -4,10 +4,31 @@ import android.graphics.Picture;
 
 import java.util.ArrayList;
 
-/**
- * Created by Yishuo Wang on 2015/10/5.
- */
 public class Book implements Shareable, Comparable<Book> {
+    /**
+     * @Author: Charles Laing
+     * @version: 1.0.0
+     * Book is the object that makes up the inventory class.
+     * It contains:
+     *  Title
+     *  Genre
+     *  Author
+     *  Publisher
+     *  Photo
+     *  Quality
+     *  Quantity
+     *  Category
+     *  Comment
+     *  Shareable?
+     *
+     * Book book = new Book(){
+     *     book.updateTitle("Harry Potter");
+     *     book.updateAuthor("J.K Rowling");
+     *     ...
+     *
+     * }
+     *
+     */
     private String bookName; // Required
     private String genre; //String to Contain Genre. A spinner is used to set this, so don't worry about the 10 requirement.
     private int genreID; // Required
@@ -15,34 +36,60 @@ public class Book implements Shareable, Comparable<Book> {
     private String publisher;
     private ArrayList <Picture> covers; //List of the pictures. Be sure to use the photo's add method to ensure that the photos are under the size req.
     private String ISBN;
-    private int quality; // Required
-    private int quantity; //Value Between 1 and 5
+    private int quality; // value between 1 and 5
+    private int quantity;
     private String category; //Some ~10 values
     private String comment; //Arbitrary string
     private boolean sharable = false; //Default items to false.
 
-    public boolean isShareable(){return this.sharable;}
+    public boolean isShareable(){
+        /**
+         * Returns a Boolean representing if the book is shareable or not.
+         *
+         */
+        return this.sharable;
+    }
 
-    public void setShareable(boolean setValue){this.sharable = setValue;}
+    public void setShareable(boolean setValue){
+        /**
+         * Used to set the value of whether the book is shareable or not.
+         */
+        this.sharable = setValue;
+    }
 
     public void updateTitle(String title){
+        /**
+         * Updates the title of the Book. Accepts a String Object.
+         */
         this.bookName = title;
 
     }
 
     public void updateAuthor(String author){
+        /**
+         * Updates the stored Author. Accepts a String Object.
+         */
         this.author = author;
     }
 
     public String getName(){
+        /**
+         * Updates the Name of the Book. Accepts a String Object.
+         */
         return this.bookName;
     }
 
     public int compareTo(Book book){
+        /**
+         * Implementation of CompareTo, used for Shareable. This method compares two books together by Title.
+         */
         return bookName.compareTo(book.getName());
     } //Method is used for searching, and searchable, and comparable.
 
     public void setQuality(int setCondition){
+        /**
+         * Sets the Quality of the Book on a scale of 1-5. Accepts an int.
+         */
         //We have to ensure that the value of the condition never exceeds a certain value
         if (setCondition > 5){
             this.quality = 5;
@@ -56,48 +103,91 @@ public class Book implements Shareable, Comparable<Book> {
     } //Sets to a value between 1 and 5
 
     public int getQuality(){
+        /**
+         * Returns the Stored Quality value as an int
+         */
         return this.quality;
     } //Returns the Quality Integer
 
     public void updateCategory(String updatedcat){
+        /**
+         * Updates the Category of the book to an Arbitrary String.
+         * The 10 Categories rule is imposed by the design of the Sorting, adding, and Browsing pages.
+         */
         this.category = updatedcat;
     }
 
     public String getCategory(){
+        /**
+         * Returns a String of the Book's Stored Category.
+         */
         return this.category;
     }
 
     public void updateQuantity(int updateint){
+        /**
+         * Updates the Quantity. Accepts and int.
+         */
         this.quantity = updateint;
     }
 
-    public int getQuantity(){return quantity;}
+    public int getQuantity(){
+        /**
+         * Returns an int representing a quantity of books.
+         */
+        return quantity;
+    }
 
     public String getComment() {
+        /**
+         * Returns a string representing the User comment on the Book.
+         */
         return comment;
     }
 
     public void updateComment(String comment) {
+        /**
+         * Accepts a string and updates the comment.
+         */
         this.comment = comment;
     }
 
     public int getGenreID() {
+        /**
+         * Returns an int of the GenreID
+         */
         return genreID;
     }
 
     public String getBookName() {
+        /**
+         * Returns a Book Name String.
+         */
         return bookName;
     }
 
     public String getAuthor() {
+        /**
+         * Returns a string representing the Author.
+         */
         return author;
     }
 
     public String getPublisher() {
+        /**
+         * Returns a String representing the Publisher.
+         */
         return publisher;
     }
 
     public String toString(){
+        /**
+         * Used to accomodate the use of an ArrayAdapter. Returns a condensed, two line string
+         * With the Author and the Title laid out like so
+         *
+         * Title: Harry Potter and the Philosopher's Stone
+         * Author: J. K. Rowling
+         */
         return "Title: "+this.getName()+"\n"+"Author: "+this.getAuthor();
     }
 }
