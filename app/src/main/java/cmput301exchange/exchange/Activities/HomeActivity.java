6ed4cfe9c1040410cd0622c9dc831ced9a4e5dfd
k----n <kalvin@kalvineng.com>
2015-11-05 11:49:19 -0700

    Added menu to home activity

--- app/src/main/java/cmput301exchange/exchange/Activities/HomeActivity.java
+++ app/src/main/java/cmput301exchange/exchange/Activities/HomeActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import cmput301exchange.exchange.EditProfile;
 import cmput301exchange.exchange.R;
 import cmput301exchange.exchange.ViewPerson;
 
@@ -48,6 +49,15 @@ public class HomeActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        // TODO pass through intent the current user's profile
+        if (id == R.id.action_view_profile) {
+            Intent intent = new Intent(this, ViewPersonActivity.class);
+            startActivity(intent);
+        }
+        if (id == R.id.action_edit_profile) {
+            Intent intent = new Intent(this, EditProfileActivity.class);
+            startActivity(intent);
+        }
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, ConfigurationActivity.class);
             startActivity(intent);
