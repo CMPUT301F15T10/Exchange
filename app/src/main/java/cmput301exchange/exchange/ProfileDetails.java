b6bd68de05cbc5655943ebe45ef20392c059d84b
viceblue <zhuhang0724@gmail.com>
2015-11-05 11:22:18 -0700

    fix friend test

--- app/src/main/java/cmput301exchange/exchange/ProfileDetails.java
+++ app/src/main/java/cmput301exchange/exchange/ProfileDetails.java
@@ -4,7 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.R;
+
 
 public class ProfileDetails extends ActionBarActivity {
 
