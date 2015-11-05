ca2718333b8f1ded5742a5a05315a154e93a4f0e
k----n <kalvin@kalvineng.com>
2015-11-05 13:04:15 -0700

    Updated login UI

--- app/src/main/java/cmput301exchange/exchange/Activities/Login.java
+++ app/src/main/java/cmput301exchange/exchange/Activities/Login.java
@@ -16,7 +16,7 @@ public class Login extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         /*
         Button testEmailButton = (Button) findViewById(R.id.testEmailButton);
