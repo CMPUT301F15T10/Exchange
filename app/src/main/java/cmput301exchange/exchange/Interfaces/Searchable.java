51669c29c2886940cad9d9b5dcde30e637d36f3b
BaihongQi <bq@ualberta.ca>
2015-11-05 22:16:59 -0700

    delete itemtest

---
+++ app/src/main/java/cmput301exchange/exchange/Interfaces/Searchable.java
@@ -0,0 +1,8 @@
+package cmput301exchange.exchange.Interfaces;
+
+/**
+ * Created by Charles on 10/16/2015.
+ */
+public interface Searchable {
+    Object Search(String searchString); //All classes that use searchable must have a search method.
+}
---
+++ app/src/main/java/cmput301exchange/exchange/Interfaces/Shareable.java
@@ -0,0 +1,9 @@
+package cmput301exchange.exchange.Interfaces;
+
+/**
+ * Created by Charles on 10/16/2015.
+ */
+public interface Shareable {
+    boolean isShareable();
+    void setShareable(boolean setValue);
+}
