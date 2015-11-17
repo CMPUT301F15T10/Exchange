c8598f5bad1b32eab8447a90639e2b58cb5cffd7
viceblue <zhuhang0724@gmail.com>
2015-11-17 13:32:28 -0700

    finish login ui test

---
+++ app/src/androidTest/java/cmput301exchange/exchange/test_Login_page.java
@@ -0,0 +1,65 @@
+package cmput301exchange.exchange;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+
+import cmput301exchange.exchange.Activities.HomeActivity;
+import cmput301exchange.exchange.Activities.Login;
+
+/**
+ * Created by hzhu6 on 11/17/15.
+ */
+public class test_Login_page extends ActivityInstrumentationTestCase2 {
+    private Activity activity;
+    private Button login_button;
+    private EditText login_text;
+
+    public test_Login_page() {
+        super(cmput301exchange.exchange.Activities.Login.class);
+    }
+
+    public void testStart() throws Exception {
+        Activity activity = getActivity();
+    }
+
+    public void testlogin(){
+        activity= (Login)getActivity();
+        login_button = (Button)activity.findViewById(R.id.button);
+        login_text = (EditText)activity.findViewById(R.id.LoginName);
+        final String acc = "acc";
+       activity.runOnUiThread(new Runnable() {
+           public void run() {
+               login_text.setText(acc);
+           }
+       });
+
+        getInstrumentation().waitForIdleSync();
+
+        Instrumentation.ActivityMonitor receiverActivityMonitor =
+                getInstrumentation().addMonitor(HomeActivity.class.getName(),
+                        null, false);
+        activity.runOnUiThread(new Runnable() {
+            public void run() {
+                login_button.performClick();
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+
+
+        HomeActivity receiverActivity = (HomeActivity)
+                receiverActivityMonitor.waitForActivityWithTimeout(1000);
+
+        assertNotNull("ReceiverActivity is null", receiverActivity);
+        assertEquals("Monitor for ReceiverActivity has not been called",
+                1, receiverActivityMonitor.getHits());
+        assertEquals("Activity is of wrong type",
+                HomeActivity.class, receiverActivity.getClass());
+        getInstrumentation().removeMonitor(receiverActivityMonitor);
+
+    }
+
+
+}
---
+++ app/src/androidTest/java/cmput301exchange/exchange/test_add_item_ui.java
@@ -0,0 +1,82 @@
+package cmput301exchange.exchange;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+
+import cmput301exchange.exchange.Activities.AddItemActivity;
+import cmput301exchange.exchange.Activities.InventoryActivity;
+
+/**
+ * Created by hzhu6 on 11/17/15.
+ */
+public class test_add_item_ui extends ActivityInstrumentationTestCase2 {
+    private Activity activity;
+    private EditText editname;
+    private EditText editauthor;
+    private EditText editquality;
+    private EditText editquantity;
+    private EditText editcomments;
+    private Button   add_button;
+
+    public test_add_item_ui() {
+        super(cmput301exchange.exchange.Activities.AddItemActivity.class);
+    }
+
+    public void testStart() throws Exception {
+        Activity activity = getActivity();
+    }
+
+
+    public void testAddValid(){
+        activity = (AddItemActivity)getActivity();
+        editname = (EditText)activity.findViewById(R.id.editName);
+        editauthor = (EditText)activity.findViewById(R.id.editAuthor);
+        editquality = (EditText)activity.findViewById(R.id.editQuality);
+        editquantity = (EditText)activity.findViewById(R.id.editQuantity);
+        editcomments = (EditText)activity.findViewById(R.id.editComment);
+        add_button = (Button)activity.findViewById(R.id.add_button);
+
+        activity.runOnUiThread(new Runnable() {
+            public void run() {
+                editname.setText("abookname");
+                editauthor.setText("anauthor");
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+
+        Instrumentation.ActivityMonitor receiverActivityMonitor =
+                getInstrumentation().addMonitor(InventoryActivity.class.getName(),
+                        null, false);
+
+        activity.runOnUiThread(new Runnable() {
+            public void run() {
+                add_button.performClick();
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+
+        InventoryActivity receiverActivity = (InventoryActivity)
+                receiverActivityMonitor.waitForActivityWithTimeout(1000);
+
+        assertNotNull("ReceiverActivity is null", receiverActivity);
+        assertEquals("Monitor for ReceiverActivity has not been called",
+                1, receiverActivityMonitor.getHits());
+        assertEquals("Activity is of wrong type",
+                InventoryActivity.class, receiverActivity.getClass());
+        getInstrumentation().removeMonitor(receiverActivityMonitor);
+
+
+
+
+
+
+
+
+    }
+
+
+}
