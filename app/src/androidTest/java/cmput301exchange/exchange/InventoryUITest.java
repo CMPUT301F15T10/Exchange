546b8eb61c991b4e5b5eb340f0ce06b9f9b50ce0
k----n <kalvin@kalvineng.com>
2015-11-18 20:13:23 -0700

    Testing more elements in Inventory

--- app/src/androidTest/java/cmput301exchange/exchange/InventoryUITest.java
+++ app/src/androidTest/java/cmput301exchange/exchange/InventoryUITest.java
@@ -5,6 +5,7 @@ import android.app.Instrumentation;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.KeyEvent;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.google.gson.Gson;
@@ -16,6 +17,8 @@ public class InventoryUITest extends ActivityInstrumentationTestCase2<InventoryA
     private Activity inventory;
     private Instrumentation mInstrumentation;
     private Spinner inventorySpinner;
+    private Spinner inventoryViewSpinner;
+    private ListView itemList;
     public Inventory inventory1 = new Inventory();
 
     public InventoryUITest() {
@@ -28,12 +31,15 @@ public class InventoryUITest extends ActivityInstrumentationTestCase2<InventoryA
         String json = gson.toJson(inventory1);
 
         Intent intent = new Intent(); //Create a new Intent
-        intent.putExtra("Inventory",json); //Pack the Intent with a blank Inventory
+        intent.putExtra("Inventory", json); //Pack the Intent with a blank Inventory
         setActivityIntent(intent); //Spoof the Intent
 
         inventory = getActivity(); // get a references to the app under test
 
         inventorySpinner = (Spinner) inventory.findViewById(R.id.spinner1);
+        inventoryViewSpinner = (Spinner) inventory.findViewById(R.id.View_spinner);
+        itemList = (ListView) inventory.findViewById(R.id.listView3);
+
         mInstrumentation = getInstrumentation();
     }
 
@@ -52,6 +58,52 @@ public class InventoryUITest extends ActivityInstrumentationTestCase2<InventoryA
         mInstrumentation.waitForIdleSync();
 
         this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+
+    }
+
+    public void testInventoryViewSpinner() {
+
+        inventory.runOnUiThread(new Runnable() {
+            public void run() {
+                inventoryViewSpinner.requestFocus();
+            }
+        });
+
+        mInstrumentation.waitForIdleSync();
+
+        this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+
+    }
+
+    public void testItemList() {
+
+        inventory.runOnUiThread(new Runnable() {
+            public void run() {
+                itemList.requestFocus();
+            }
+        });
+
+        mInstrumentation.waitForIdleSync();
+
+        this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+
+        mInstrumentation.waitForIdleSync();
+
+        this.sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
+
+        mInstrumentation.waitForIdleSync();
+
+        this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+
+        mInstrumentation.waitForIdleSync();
+
+        this.sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
+
+        mInstrumentation.waitForIdleSync();
+
+        this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+
+        mInstrumentation.waitForIdleSync();
 
     }
 }
