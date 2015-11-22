32dd4a7889a2440a1ddea4a8ff841b496b43d035
name <kstem@users.noreply.github.com>
2015-11-21 19:56:33 -0700

    Update Photo.java

--- app/src/main/java/cmput301exchange/exchange/Photo.java
+++ app/src/main/java/cmput301exchange/exchange/Photo.java
@@ -1,95 +1,19 @@
 package cmput301exchange.exchange;
-
-import android.graphics.Bitmap;
-import android.view.View;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Observable;
-
-
-import cmput301exchange.exchange.Interfaces.Observer;
-/**
+/*
  * @author Kaleb
  * Represents a photo that is attached to each item
  */
 
-public class Photo extends Observable {
+import java.util.ArrayList;
 
-    private volatile ArrayList<Observer> observers = new ArrayList<Observer>();
-    private static final String RESOURCE_URL = "http://cmput301.softwareprocess.es:8080/testing/";
+public class Photo {
 
-
-   // influenced by: http://www.mkyong.com/java/how-to-get-file-size-in-java/
-    // TODO: adapt to get size pf user's photo
-    public double photoSize(Photo photo){
-        /**
-         * Returns the size of a user's photo
-         * @param photo a photo object
-         */
-        File pic = new File("/cshome/kstember/Desktop/testButton.jpg");
-        // note this only works on my (kaleb's) computer in CSC.
-        File pic2 = new File("/Exchange/app/src/main/res/drawable/testphoto.jpg");
-        // hopefully works on all computers with 'testphoto.jpg' in drawable folder
-        double bytes = 0;
-        if(pic.exists()) {
-            // bytes = photo.length() once we get there
-            bytes = pic.length();
-        } // else { throw an error}
-        return bytes;
+    private ArrayList<byte[]> photo;
+    public Photo() {
+        photo = new ArrayList<>();
     }
 
-
-    // code written by github user Santiago
-    //http://stackoverflow.com/questions/31978299/how-to-covert-image-view-into-bitmap-in-android-studio
-       /* public static Bitmap getScreenViewBitmap(View v) {
-            v.setDrawingCacheEnabled(true);
-
-            // this is the important code :)
-            // Without it the view will have a dimension of 0,0 and the bitmap will be null
-            v.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-            v.layout(0, 0, v.getMeasuredWidth(), v.getMeasuredHeight());
-
-            v.buildDrawingCache(true);
-            Bitmap b = Bitmap.createBitmap(v.getDrawingCache());
-            v.setDrawingCacheEnabled(false); // clear drawing cache
-
-            return b;
-        }
-*/
-    public Photo() {}
-
-// code below written by joshua2ua on github 
-// https://github.com/joshua2ua/lonelyTwitter
-    //@Override
-    public void addObserver(Observer o) {
-        observers.add(o);
+    public void addPhoto(byte[] b)  {
+        photo.add(0, b);
     }
-
-    // @Override
-    public void deleteObserver(Observer o) {
-        observers.remove(o);
-    }
-
-    @Override
-    public void notifyObservers() {
-        for (Observer o : observers) {
-            o.update(/*this*/);
-        }
-    }
-
-    public String getResourceUrl() {
-        return RESOURCE_URL;
-    }
-
-   /* public String getSearchUrl() {
-        return SEARCH_URL;
-    }
-*/
-    /**
-     * Java wants this, we don't need it for Gson/Json
-     */
-    private static final long serialVersionUID = 3199561696102797345L;
-
 }
