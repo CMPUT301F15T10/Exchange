e1397b43837ec21cc097590943438ec3398ade71
kstem <kstember@ualberta.ca>
2015-11-30 11:26:28 -0700

    formatting

--- app/src/main/java/cmput301exchange/exchange/Activities/AddBookActivity.java
+++ app/src/main/java/cmput301exchange/exchange/Activities/AddBookActivity.java
@@ -8,42 +8,33 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
-import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.Spinner;
-
 import com.google.gson.Gson;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Random;
-
 import cmput301exchange.exchange.Activities.Adapters.PhotoAdapter;
 import cmput301exchange.exchange.Book;
 import cmput301exchange.exchange.Inventory;
 import cmput301exchange.exchange.R;
 import cmput301exchange.exchange.Serializers.DataIO;
 
-public class AddBookActivity extends ActionBarActivity {
+public class AddBookActivity extends AppCompatActivity {
 
     private EditText name, author, quality, quantity, comments;
     private ImageButton image;
